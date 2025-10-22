c41b4e4e9ad7b3667394f8c5a296e4a05e860f16
fabio.nogueira <fabio.nogueira@studocu.com>
2025-10-22 11:15:02 +0200

    datahub

--- deploy/ec2-deploy.sh
+++ deploy/ec2-deploy.sh
@@ -48,7 +48,7 @@ echo "🚀 Starting DataHub services..."
 set -a
 source "$ENV_FILE"
 set +a
-docker compose up -d
+docker compose up  --env-file /home/ubuntu/datahub/.env up -d
 
 
 echo "⏳ Waiting for services to be healthy..."
